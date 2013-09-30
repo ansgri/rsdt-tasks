ab6de7dbfd9647b2e659c370d575d1a2a1d19b95
Anton Grigoryev <me@ansgri.com>
2013-09-30 23:43:52 +0400

    NEW: rsdt-tasks repository based on cpp-boilerplate-minimal

--- cmake/constants.cmake
+++ cmake/constants.cmake
@@ -1,36 +1,36 @@
 # --- architecture ---
 if(WIN32)
-  set(CPPBP_OPERATING_SYSTEM "win")
+  set(RSDT_TASKS_OPERATING_SYSTEM "win")
 elseif(UNIX)
-  set(CPPBP_OPERATING_SYSTEM "linux")
+  set(RSDT_TASKS_OPERATING_SYSTEM "linux")
 else()
   message(FATAL_ERROR "Unknown operating system")
 endif()
 
 if(CMAKE_SIZEOF_VOID_P EQUAL "4")
-  set(CPPBP_CPU_BITNESS "32")
+  set(RSDT_TASKS_CPU_BITNESS "32")
 elseif(CMAKE_SIZEOF_VOID_P EQUAL "8")
-  set(CPPBP_CPU_BITNESS "64")
+  set(RSDT_TASKS_CPU_BITNESS "64")
 else()
   message(FATAL_ERROR "Unsupported CPU bitness")
 endif()
 
-set(CPPBP_ARCH ${CPPBP_OPERATING_SYSTEM}${CPPBP_CPU_BITNESS})
-message(STATUS "Target architecture: ${CPPBP_ARCH}")
+set(RSDT_TASKS_ARCH ${RSDT_TASKS_OPERATING_SYSTEM}${RSDT_TASKS_CPU_BITNESS})
+message(STATUS "Target architecture: ${RSDT_TASKS_ARCH}")
 
 # --- directories ---
-set(CPPBP_ROOT ${cppbp_SOURCE_DIR})
-set(CPPBP_INCLUDE_ROOT ${CPPBP_ROOT}/include)
-set(CPPBP_LIB_ROOT ${CPPBP_ROOT}/lib/${CPPBP_ARCH})
+set(RSDT_TASKS_ROOT ${rsdt_tasks_SOURCE_DIR})
+set(RSDT_TASKS_INCLUDE_ROOT ${RSDT_TASKS_ROOT}/include)
+set(RSDT_TASKS_LIB_ROOT ${RSDT_TASKS_ROOT}/lib/${RSDT_TASKS_ARCH})
 if(WIN32 AND NOT MSVC_IDE)
-  set(CPPBP_LIB_ROOT ${CPPBP_LIB_ROOT} ${CPPBP_LIB_ROOT}/${CMAKE_BUILD_TYPE})
+  set(RSDT_TASKS_LIB_ROOT ${RSDT_TASKS_LIB_ROOT} ${RSDT_TASKS_LIB_ROOT}/${CMAKE_BUILD_TYPE})
 endif()
 
 
 
 # --- lists of libraries for linking ---
 if(WIN32)
-  set(CPPBP_OPENCV_LIBS 
+  set(RSDT_TASKS_OPENCV_LIBS 
     optimized opencv_core242
     optimized opencv_highgui242
     optimized opencv_imgproc242
@@ -41,7 +41,7 @@ if(WIN32)
     debug opencv_video242d
   )
 elseif(UNIX)
-  set(CPPBP_OPENCV_LIBS 
+  set(RSDT_TASKS_OPENCV_LIBS 
     opencv_core
     opencv_highgui
     opencv_imgproc
@@ -50,17 +50,17 @@ elseif(UNIX)
 endif()
 
 if(WIN32)
-  set(CPPBP_JRTP_LIBS 
+  set(RSDT_TASKS_JRTP_LIBS 
     optimized jrtplib
     debug jrtplib_d
   )
 elseif(UNIX)
-  set(CPPBP_JRTP_LIBS
+  set(RSDT_TASKS_JRTP_LIBS
     jrtp
   )
 endif()
 
-set(CPPBP_FFMPEG_LIBS
+set(RSDT_TASKS_FFMPEG_LIBS
   avcodec
   avcore
   avdevice
